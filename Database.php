4902da9e4f5a6f4b08edfc789840bb7173213843
Ralphjc24 <74389994+Ralphjc24@users.noreply.github.com>
2025-04-05 21:53:32 -0400

    Add files via upload
    The connection to the SQL DB

---
+++ Database.php
@@ -0,0 +1,20 @@
+<?php
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "gymmembership";
+    $conn = "";
+
+
+    $conn = mysqli_connect($db_server,
+                            $db_user,
+                            $db_pass,
+                            $db_name);
+
+    if($conn){
+            echo "Connected";
+    }
+    else{
+            echo "Not Connected";
+    }
+?>
